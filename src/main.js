bfa1a9e185a8008a9a5b40e92adfb1d143ce0a34
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2021-10-14 23:18:40 +0100

    Added addItemPreview() to create Item Previews dynamically

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-console.log("Works");
+const stockListItems = document.querySelector(".stock-list__items");
+const itemPreviewTemplate = document.getElementById("item-preview-template");
+
+const addItemPreview = ({ image, name, colour, material, quantity }) => {
+	const itemPreview = itemPreviewTemplate.content.cloneNode(true).firstElementChild;
+
+	// Item Name
+	itemPreview.children[0].src = "img/" + image;
+	itemPreview.children[1].innerHTML = name;
+	itemPreview.children[3].innerHTML = colour;
+	itemPreview.children[5].innerHTML = material;
+	itemPreview.children[7].innerHTML = quantity;
+
+	stockListItems.appendChild(itemPreview);
+};
+
+
+
+// Add Test Previews
+const numTestPreviews = 10;
+const testItem = {
+	image: "aa-logo-stamp.png",
+	name: "Stripy Grey Pattern",
+	colour: "Grey",
+	material: "Cotton",
+	quantity: "8m",
+};
+
+for (let i = 0; i < numTestPreviews; i++) addItemPreview(testItem);
