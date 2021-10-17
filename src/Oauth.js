982fc0d6461f35f2710eb6e6d32dd0901c3c4d4e
PatrickJMurchie <patrickjohnmurchie@gmail.com>
2021-10-17 21:05:48 +0100

    Added a simple google Oauth

---
+++ src/Oauth.js
@@ -0,0 +1,23 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    if (profile.getEmail() == "patrickjohnmurchie@gmail.com" ||
+        profile.getEmail() == "rmackenzie88@gmail.com" ||
+        profile.getEmail() == "callummcintosh6675@gmail.com") {
+        window.location.href = "./index.html"
+    } else {}
+}
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function() {
+        console.log('User signed out.');
+        window.location.href = "./LogIn.html"
+    });
+
+}
