f70662cbae8ba17a1219e06267954b622b651a13
PatrickJMurchie <patrickjohnmurchie@gmail.com>
2021-10-24 15:07:11 +0100

    changes backend

--- config.js
+++ config.js
@@ -1,36 +1,37 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+config = {
     host: "localhost",
     user: "root",
     password: "",
     database: "acmeAtelierInventory"
-});
+}
 
+var con = mysql.createConnection(config);
 con.connect(function(err) {
     if (err) throw err;
     //Select all fablic names and return the result object:
     con.query("SELECT names FROM tblFabic", function(err, result, fields) {
             if (err) throw err;
-            var names = console.log(result);
+            var names = result;
         })
         //Select all fablic quatities and return the result object:
     con.query("SELECT quatity FROM tblFabic", function(err, result, fields) {
             if (err) throw err;
-            var quatity = console.log(result);
+            var quatity = result;
         })
         //Select all image names and return the result object:
     con.query("SELECT image1 FROM tblFabic", function(err, result, fields) {
         if (err) throw err;
-        var image1 = console.log(result);
+        var image1 = result;
     })
     con.query("SELECT image2 FROM tblFabic", function(err, result, fields) {
         if (err) throw err;
-        var image2 = console.log(result);
+        var image2 = result;
     })
     con.query("SELECT image3 FROM tblFabic", function(err, result, fields) {
         if (err) throw err;
-        var image3 = console.log(result);
+        var image3 = result;
     })
 
 })
