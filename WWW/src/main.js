154328b75e6ba02c6652e76a9dded430fa71ab52
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2021-10-25 04:21:56 +0100

    Added item details page and mobile styling

--- WWW/src/main.js
+++ WWW/src/main.js
@@ -1,29 +1,16 @@
-//
 //import { names } from './config.js';
-const stockListItems = document.querySelector(".stock-list__items");
-const itemPreviewTemplate = document.getElementById("item-preview-template");
-const addItemPreview = ({ image, name, colour, material, quantity }) => {
-    const itemPreview = itemPreviewTemplate.content.cloneNode(true).firstElementChild;
-
-    // Item Name
-    itemPreview.children[0].src = "img/" + image;
-    itemPreview.children[1].innerHTML = name;
-    itemPreview.children[3].innerHTML = colour;
-    itemPreview.children[5].innerHTML = material;
-    itemPreview.children[7].innerHTML = quantity;
-
-    stockListItems.appendChild(itemPreview);
-};
+import { addItemPreview } from "./ui.js";
 
 // Add Test Previews
 const numTestPreviews = 10;
 const testItem = {
-    image: "aa-logo-stamp.png",
+    images: [
+			"tartan.jpg",
+		],
     name: "Tartan",
-    colour: "Grey",
-    material: "Cotton",
+    colour: "Green",
+    material: "Wool (Light)",
     quantity: "8m",
 };
-
 
 for (let i = 0; i < numTestPreviews; i++) addItemPreview(testItem);
---
+++ WWW/src/stock.js
@@ -0,0 +1,21 @@
+export const getStockData = () => {
+	const data = {};
+
+	return data;
+};
+
+export const getStockItem = (id) => {
+
+};
+
+export const addStockItem = (data) => {
+
+};
+
+export const updateStockItem = (id, data) => {
+
+};
+
+export const deleteStockItem = (id) => {
+
+};
