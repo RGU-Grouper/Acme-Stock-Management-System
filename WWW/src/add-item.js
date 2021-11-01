be627c0faf4d2b5bf28ed67377f0e9b596353b19
PatrickJMurchie <patrickjohnmurchie@gmail.com>
2021-11-01 16:32:11 +0000

    add item

--- WWW/src/add-item.js
+++ WWW/src/add-item.js
@@ -1,44 +1,44 @@
 const $tagTemplate = document.getElementById("tag-template");
 
 const addTagToList = ($list, tag) => {
-	const $tag = $tagTemplate.content.cloneNode(true).firstElementChild;
+    const $tag = $tagTemplate.content.cloneNode(true).firstElementChild;
 
-	$tag.children[0].innerText = tag;
+    $tag.children[0].innerText = tag;
 
-	$tag.children[1].addEventListener("click", (event) => {
-		$list.removeChild($tag);
-	});
+    $tag.children[1].addEventListener("click", (event) => {
+        $list.removeChild($tag);
+    });
 
-	$list.appendChild($tag);
+    $list.appendChild($tag);
 };
 
 const createTagSelectItem = (tag, onClickCallback) => {
-	const $tagItem = document.createElement("li");
-	$tagItem.innerText = tag;
-	$tagItem.addEventListener("click", onClickCallback);
-	return $tagItem;
+    const $tagItem = document.createElement("li");
+    $tagItem.innerText = tag;
+    $tagItem.addEventListener("click", onClickCallback);
+    return $tagItem;
 };
 
 const createTagSelectList = (tags) => {
-	// Create list element
-	const $tagList = document.createElement("ul");
+    // Create list element
+    const $tagList = document.createElement("ul");
 
-	// Create list item elements
-	for (let i = 0; i < tags.length; i++) {
-		const $tagItem = createTagSelectItem(tags[i], (event) => {
-			// On Click...
+    // Create list item elements
+    for (let i = 0; i < tags.length; i++) {
+        const $tagItem = createTagSelectItem(tags[i], (event) => {
+            // On Click...
 
-		});
-		$tagList.appendChild($tagItem);
-	}
-	
-	// Create Add New list item element
-	const $addTag = createTagSelectItem("Add New...", (event) => {
-		// On Click...
-		
-	});
-	$tagList.appendChild($addTag);
+        });
+        $tagList.appendChild($tagItem);
+    }
 
-	// Return the completed tag list element
-	return $tagList;
+    // Create Add New list item element
+    const $addTag = createTagSelectItem("Add New...", (event) => {
+        // On Click...
+
+    });
+    $tagList.appendChild($addTag);
+
+    // Return the completed tag list element
+    return $tagList;
 };
