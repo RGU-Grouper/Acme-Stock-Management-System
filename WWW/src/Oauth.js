6492d6450787a6ba6a355e80f32fff6ca8e57f28
PatrickJMurchie <patrickjohnmurchie@gmail.com>
2021-11-07 02:55:31 +0000

    commit

--- WWW/src/Oauth.js
+++ WWW/src/Oauth.js
@@ -4,31 +4,36 @@ function onSignIn(googleUser) {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
     //list the emails that can login to the website using Gmail OAuth
+    //list the emails that can login to the website using Gmail OAuth
+
+    //list the emails that can login to the website using Gmail OAuth
+
     if (profile.getEmail() == "patrickjohnmurchie@gmail.com" ||
         profile.getEmail() == "rmackenzie88@gmail.com" ||
         profile.getEmail() == "callummcintosh6675@gmail.com" ||
         profile.getEmail() == "helloacmeatelier@gmail.com" ||
         profile.getEmail() == "ryancitomlinson@gmail.com"
     ) {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function() {
+            console.log('User signed out.');
+
+        });
         window.location.href = "./index.html"
+
     } else {
         let done = confirm("You don't have permission to access this.")
         if (done) {
             window.location.href = "https://www.acmeatelier.co.uk/"
+            signOut();
         } else {
-            alert("Please try again.")
+            alert("Please try again.");
+
         }
-
-
     }
 }
-
 //Function to signOut
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
-        console.log('User signed out.');
-        window.location.href = "./LogIn.html"
-    });
+    window.location.href = "./logIn.html"
 
 }
--- WWW/src/http.js
+++ WWW/src/http.js
@@ -1,138 +1,137 @@
 // Stock Data
 export const getStockData = () => {
-	const data = [
-		{
-			id: "0",
-			name: "Tartan 1",
-			quantity: 1,
-			images: ["tartan.jpg", "red.jpg", "purple.jpg"],
-			tagLists: {
-				materials: ["Wool"],
-				mainColours: ["Red"],
-				highlightColours: [],
-			},
-		},
-		{
-			id: "1",
-			name: "Tartan 2",
-			quantity: 2,
-			images: ["tartan.jpg"],
-			tagLists: {
-				materials: ["Cotton"],
-				mainColours: ["Green"],
-				highlightColours: ["Red"],
-			},
-		},
-		{
-			id: "2",
-			name: "Tartan 3",
-			quantity: 3,
-			images: ["red.jpg"],
-			tagLists: {
-				materials: ["Wool", "Cotton"],
-				mainColours: [],
-				highlightColours: ["Blue"],
-			},
-		},
-		{
-			id: "3",
-			name: "Tartan 4",
-			quantity: 4,
-			images: ["purple.jpg"],
-			tagLists: {
-				materials: ["Nylon", "Linen", "Wool", "Cotton"],
-				mainColours: ["Yellow", "Purple", "Green"],
-				highlightColours: ["Red", "Blue"],
-			},
-		},
-		{
-			id: "4",
-			name: "Find Me",
-			quantity: 5,
-			images: ["tartan.jpg", "red.jpg", "purple.jpg"],
-			tagLists: {
-				materials: [],
-				mainColours: ["Brown"],
-				highlightColours: ["Brown"],
-			},
-		},
-		{
-			id: "5",
-			name: "Tartan 6",
-			quantity: 6,
-			images: ["tartan.jpg"],
-			tagLists: {
-				materials: ["Wool"],
-				mainColours: ["Red", "Green"],
-				highlightColours: ["Green", "Red"],
-			},
-		}
-	];
-	return data.concat(data.concat(data));
+    const data = [{
+            id: "0",
+            name: "Tartan 1",
+            quantity: 1,
+            images: ["tartan.jpg", "red.jpg", "purple.jpg"],
+            tagLists: {
+                materials: ["Wool"],
+                mainColours: ["Red"],
+                highlightColours: [],
+            },
+        },
+        {
+            id: "1",
+            name: "Tartan 2",
+            quantity: 2,
+            images: ["tartan.jpg"],
+            tagLists: {
+                materials: ["Cotton"],
+                mainColours: ["Green"],
+                highlightColours: ["Red"],
+            },
+        },
+        {
+            id: "2",
+            name: "Tartan 3",
+            quantity: 3,
+            images: ["red.jpg"],
+            tagLists: {
+                materials: ["Wool", "Cotton"],
+                mainColours: [],
+                highlightColours: ["Blue"],
+            },
+        },
+        {
+            id: "3",
+            name: "Tartan 4",
+            quantity: 4,
+            images: ["purple.jpg"],
+            tagLists: {
+                materials: ["Nylon", "Linen", "Wool", "Cotton"],
+                mainColours: ["Yellow", "Purple", "Green"],
+                highlightColours: ["Red", "Blue"],
+            },
+        },
+        {
+            id: "4",
+            name: "Find Me",
+            quantity: 5,
+            images: ["tartan.jpg", "red.jpg", "purple.jpg"],
+            tagLists: {
+                materials: [],
+                mainColours: ["Brown"],
+                highlightColours: ["Brown"],
+            },
+        },
+        {
+            id: "5",
+            name: "Tartan 6",
+            quantity: 6,
+            images: ["tartan.jpg"],
+            tagLists: {
+                materials: ["Wool"],
+                mainColours: ["Red", "Green"],
+                highlightColours: ["Green", "Red"],
+            },
+        }
+    ];
+    return data.concat(data.concat(data));
 
-	// const res = await fetch("/stock");
-	// const data = await res.json();
-	// return data;
+    // const res = await fetch("/stock");
+    // const data = await res.json();
+    // return data;
 };
 
-export const getStockItem = async (id) => {
-	const res = await fetch(`/stock/${id}`);
-	const data = await res.json();
-	return data;
+export const getStockItem = async(id) => {
+    const res = await fetch(`/stock/${id}`);
+    const data = await res.json();
+    return data;
 };
 
-export const addStockItem = async (data) => {
-	const res = await fetch("/stock", {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(data),
-	});
-	return res.ok;
+export const addStockItem = async(data) => {
+    const res = await fetch("/stock", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return res.ok;
 };
 
-export const updateStockItem = async (id, data) => {
-	const res = await fetch(`/stock/${id}`, {
-		method: "PATCH",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(data),
-	});
-	return res.ok;
+export const updateStockItem = async(id, data) => {
+    const res = await fetch(`/stock/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return res.ok;
 };
 
-export const deleteStockItem = async (id) => {
-	const res = await fetch(`/stock/${id}`, { method: "DELETE" });
-	return res.ok;
+export const deleteStockItem = async(id) => {
+    const res = await fetch(`/stock/${id}`, { method: "DELETE" });
+    return res.ok;
 };
 
 // Tags
 export const getMaterialTags = () => {
-	return ["Wool", "Cotton", "Linen", "Nylon"];
-	// const res = await fetch("/tags/materials");
-	// const data = await res.json();
-	// return data;
+    return ["Wool", "Cotton", "Linen", "Nylon"];
+    // const res = await fetch("/tags/materials");
+    // const data = await res.json();
+    // return data;
 };
 
-export const addMaterialTag = async (data) => {
-	const res = await fetch("/tags/materials", {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(data),
-	});
-	return res.ok;
+export const addMaterialTag = async(data) => {
+    const res = await fetch("/tags/materials", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return res.ok;
 };
 
 export const getColourTags = () => {
-	return ["White", "Black", "Red", "Green", "Blue", "Yellow", "Orange", "Pink", "Purple", "Brown"];
-	// const res = await fetch("/tags/colours");
-	// const data = await res.json();
-	// return data;
+    return ["White", "Black", "Red", "Green", "Blue", "Yellow", "Orange", "Pink", "Purple", "Brown"];
+    // const res = await fetch("/tags/colours");
+    // const data = await res.json();
+    // return data;
 };
 
-export const addColourTag = async (data) => {
-	const res = await fetch("/tags/colours", {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify(data),
-	});
-	return res.ok;
+export const addColourTag = async(data) => {
+    const res = await fetch("/tags/colours", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+    return res.ok;
 };
