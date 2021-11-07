386cf50133bcc24e443630e0661be2c189ae1bd7
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2021-11-07 20:43:30 +0000

    Fetching stock data from server

--- controllers/stockControllers.js
+++ controllers/stockControllers.js
@@ -1,11 +1,79 @@
 const getStockData = (req, res) => {
-	res.status(200).json({
-		stock: ["testing1", "testing2", "testing3"],
-	});
+	// const data = []; // get data from database
+	const data = [
+		{
+			id: "0",
+			name: "Tartan 1",
+			quantity: 1,
+			images: ["tartan.jpg", "red.jpg", "purple.jpg"],
+			tagLists: {
+				materials: ["Wool"],
+				mainColours: ["Red"],
+				highlightColours: [],
+			},
+		},
+		{
+			id: "1",
+			name: "Tartan 2",
+			quantity: 2,
+			images: ["tartan.jpg"],
+			tagLists: {
+				materials: ["Cotton"],
+				mainColours: ["Green"],
+				highlightColours: ["Red"],
+			},
+		},
+		{
+			id: "2",
+			name: "Tartan 3",
+			quantity: 3,
+			images: ["red.jpg"],
+			tagLists: {
+				materials: ["Wool", "Cotton"],
+				mainColours: [],
+				highlightColours: ["Blue"],
+			},
+		},
+		{
+			id: "3",
+			name: "Tartan 4",
+			quantity: 4,
+			images: ["purple.jpg"],
+			tagLists: {
+				materials: ["Nylon", "Linen", "Wool", "Cotton"],
+				mainColours: ["Yellow", "Purple", "Green"],
+				highlightColours: ["Red", "Blue"],
+			},
+		},
+		{
+			id: "4",
+			name: "Find Me",
+			quantity: 5,
+			images: ["tartan.jpg", "red.jpg", "purple.jpg"],
+			tagLists: {
+				materials: [],
+				mainColours: ["Brown"],
+				highlightColours: ["Brown"],
+			},
+		},
+		{
+			id: "5",
+			name: "Tartan 6",
+			quantity: 6,
+			images: ["tartan.jpg"],
+			tagLists: {
+				materials: ["Wool"],
+				mainColours: ["Red", "Green"],
+				highlightColours: ["Green", "Red"],
+			},
+		}
+	];
+	res.status(200).json(data);
 };
 
 const addStockItem = (req, res) => {
-	const data = req.body;
+	const data = req.body.data;
+	console.log(data);
 
 	// Validate data and add to database
 
@@ -15,7 +83,27 @@ const addStockItem = (req, res) => {
 	});
 };
 
+const getStockItem = (req, res) => {
+	const id = req.body.id;
+	const data = {}; // get data from database with id
+	res.status(200).json(data);
+};
+
+const updateStockItem = (req, res) => {
+	const id = req.body.id;
+	const data = req.body.data;
+	res.status(200);
+};
+
+const deleteStockItem = (req, res) => {
+	const id = req.body.id;
+	res.status(200);
+};
+
 module.exports = {
 	getStockData,
 	addStockItem,
+	getStockItem,
+	updateStockItem,
+	deleteStockItem,
 };
